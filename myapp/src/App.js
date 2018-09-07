a600275f38c8ca631ec1cc2c023bfb68c9d6f68b
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-07 18:57:34 +0530

    pretty code

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -14,11 +14,20 @@ class App extends Component {
       <div className="App">
         <h1>Hi, This is my App created Using Create-react-app</h1>
         <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobby is xyz
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // THIS IS EXACT EQUIVALENT OF ABOVE JSX CODE. THIS IS WHAT IS DONE BEHIND THE SCENE INTERNALLY
