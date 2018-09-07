e0cdadf26c01166c340fb3aef4b32fc5c27515be
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-07 11:27:59 +0530

    jsx

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, This is my App created Using Create-react-app</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, This is my App created Using Create-react-app</h1>
+    //   </div>
+    // );
+    // THIS IS EXACT EQUIVALENT OF ABOVE JSX CODE. THIS IS WHAT IS DONE BEHIND THE SCENE INTERNALLY
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, This is my App'));
   }
 }
 
