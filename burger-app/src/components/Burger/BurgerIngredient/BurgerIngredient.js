86ab1c009cd26c52cc7ac170d5e894283a88e65b
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-09 01:18:04 +0530

    comp

---
+++ burger-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './burgerIngredient.css';
+const burgerIngredient = props => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case 'bread-bottom':
+      ingredient = <div className={classes.BreadBottom} />;
+      break;
+    case 'bread-top':
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.seed1} />
+          <div className={classes.seed2} />
+        </div>
+      );
+      break;
+    case 'meat':
+      ingredient = <div className={classes.Meat} />;
+      break;
+    case 'cheese':
+      ingredient = <div className={classes.Cheese} />;
+      break;
+    case 'bacon':
+      ingredient = <div className={classes.Bacon} />;
+      break;
+    case 'salad':
+      ingredient = <div className={classes.Salad} />;
+      break;
+    default:
+      ingredient = null;
+  }
+  return ingredient;
+};
+
+export default burgerIngredient;
