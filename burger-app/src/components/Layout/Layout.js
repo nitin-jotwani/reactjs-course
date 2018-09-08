d1eb9264e4acb9ed7cfd25a12f88f722c9f0f072
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-09 01:03:16 +0530

    container

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Classes from './Layout.css';
 const layout = props => {
   return (
     <div>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={Classes.content}>{props.children}</main>
     </div>
   );
 };
