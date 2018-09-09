79818fa3ea0fe13728cfddbec66ae15fa3c1a1d8
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-09 13:42:18 +0530

    states

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -10,7 +10,13 @@ const controls = [
 const buildControls = props => (
   <div className={classes.buildControls}>
     {controls.map(control => {
-      return <BuildControl key={control.label} label={control.label} />
+      return (
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => props.ingredientAdded(control.type)}
+        />
+      );
     })}
   </div>
 );
