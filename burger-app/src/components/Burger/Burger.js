1297033b1ad92924676e25bf3518512a026b39f1
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-09 11:18:30 +0530

    sum

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -2,17 +2,26 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = props => {
-  const transformedIngredient = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  const transformedIngredient = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
   return (
-    <div className={classes.burger}>
-      <BurgerIngredient type="bread-top" />
-      {transformedIngredient}
-      <BurgerIngredient type="bread-bottom" />
-    </div>
+      transformedIngredient.length === 0 ? (
+        <p>Please Add Ingredients</p>
+      ) : (
+        <div className={classes.burger}>
+          <BurgerIngredient type="bread-top" />
+          {transformedIngredient}
+          <BurgerIngredient type="bread-bottom" />
+        </div>
+      )
   );
 };
 
--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
+      salad: 4,
+      bacon: 3,
       cheese: 2,
-      meat: 2
+      meat: 1
     }
   }
   render() {
