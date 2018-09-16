7aa3a2627f6d945540138a386c8811cacaa7fc40
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-16 21:37:35 +0530

    update/rerender modal when its open, otherwise no need

--- burger-app/src/components/UI/Modal/Modal.js
+++ burger-app/src/components/UI/Modal/Modal.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
-const modal = props => {
-  return (
-    <div>
-      <Backdrop show={props.show} clicked={props.modalClosed}/>
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-        }}
-      >
-        {props.children}
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <div>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+          }}
+        >
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default modal;
+export default Modal;
