052a4c9000b8f3bad643f995e0640bc76f61a0d9
Nitin Jotwani <jotwani.nikki@gmail.com>
2018-09-30 15:09:58 +0530

    axios-firebase

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -31,7 +33,23 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You Continue');
+    // alert('You Continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Nitin Jotwani',
+        address: {
+          street: 'sambhaji chowk',
+          city: 'Ulhasnagar'
+        },
+        email: 'jotwani.nikki@gmail.com'
+      }
+    };
+    axios
+      .post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
   };
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
